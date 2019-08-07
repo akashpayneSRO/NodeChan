5ddbea6dd580ebedcf7d1adaf9b4be55f497169e
joshiemoore <jxm5210@g.rit.edu>
2019-08-06 23:14:28 -0400

    Add OutgoingThread

---
+++ src/OutgoingThread.java
@@ -0,0 +1,39 @@
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+import java.io.IOException;
+
+/**
+ * This class serves to send a single outgoing data packet over UDP.
+ */
+public class OutgoingThread extends Thread {
+  /** The UDP socket we're using **/
+  DatagramSocket sock;
+
+  /** The recipient of the packet **/
+  InetAddress recip;
+
+  /** The port we're sending over **/
+  int port;
+
+  /** The data packet to send **/
+  private byte[] outbytes;
+
+  public OutgoingThread(DatagramSocket sock, InetAddress recip, int port, byte[] outbytes) {
+    this.sock = sock;
+    this.recip = recip;
+    this.port = port;
+    this.outbytes = outbytes;
+  }
+
+  public void run() {
+    DatagramPacket outPacket = new DatagramPacket(outbytes, outbytes.length, recip, port);
+
+    try {
+      sock.send(outPacket);
+    } catch (IOException e) {
+      // do nothing, we don't care too heavily about every individual packet
+    }
+  }
+}
